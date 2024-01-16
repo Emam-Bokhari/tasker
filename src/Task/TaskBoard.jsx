24d5566d1636b16a1ded57b28c575e89a359d31d
Moshfiqur Rahman <moshfiqurrahman37@gmail.com>
2024-01-16 20:46:26 +0600

    Add modal in TaskBoard component

--- src/Task/TaskBoard.jsx
+++ src/Task/TaskBoard.jsx
@@ -2,10 +2,11 @@ import { Fragment, useState } from "react"
 import Searchbox from "./Searchbox"
 import TaskActions from "./TaskActions"
 import TaskList from "./TaskList"
+import AddTaskModal from "./AddTaskModal"
 
 const TaskBoard = () => {
     const defaultTask = {
-        "id":crypto.randomUUID(),
+        "id": crypto.randomUUID(),
         "title": "Integration API",
         "description": "Connect an existing API to a third-party database using secure methods and handle data exchange efficiently.",
         "tags": ['Web', 'Python', 'Api'],
@@ -15,12 +16,17 @@ const TaskBoard = () => {
 
     const [tasks, setTasks] = useState([defaultTask])
 
+    const [AddTaskShowModal, setAddTaskShowModal] = useState(false)
+
+
     return (
         <Fragment>
 
             <section className="mb-20" id="tasks">
 
                 <div className="container">
+                    {AddTaskShowModal && <AddTaskModal />}
+
 
                     {/* Searchbox */}
                     <div className="p-2 flex justify-end">
@@ -30,7 +36,7 @@ const TaskBoard = () => {
                     <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
 
                         {/* TaskActions */}
-                        <TaskActions />
+                        <TaskActions handleAddTask={() => setAddTaskShowModal(true)} />
 
                         {/* TaskList */}
                         <TaskList tasks={tasks} />
